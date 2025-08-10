7038177bbfed9396660d7c41a89aaea24d2608c1
Ali Hussain <alihussain@Alis-MBP.lan>
2025-08-09 19:33:29 -0500

    Added Parcel with other devDependencies with Notes, removed clutter

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const nestedStructure = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I am a H1 Tag"),
-    React.createElement("h2", {}, "I am a H2 Tag"),
+  React.createElement("div", { id: "child1", key: 1 }, [
+    React.createElement("h1", { key: 2 }, "I am a H1 Tag"),
+    React.createElement("h2", { key: 3 }, "I am a H2 Tag"),
   ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am a H1 Tag"),
-    React.createElement("h2", {}, "I am a H2 Tag"),
+  React.createElement("div", { id: "child2", key: 4 }, [
+    React.createElement("h1", { key: 5 }, "I am a H1 Tag"),
+    React.createElement("h2", { key: 6 }, "I am a H2 Tag"),
   ]),
 ]);
 
